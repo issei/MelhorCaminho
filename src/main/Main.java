d88daa6f476e2ccc2df1c8c3f2df76cee313966c
Mauricio Yokoyama Issei <mauricioissei@gmail.com>
2016-10-07 18:16:25 -0300

    Primeiro commit

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import br.com.issei.logistica.melhorcaminho.gui.MainWindow;
+
+public class Main {
+	public static void main(String[] args) {
+		MainWindow main = new MainWindow();
+		main.setVisible(true);
+	}
+}
